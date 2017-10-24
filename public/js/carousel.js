f9c5588ee54453c263ade0d93f8b004db5e8e21c
karenF <karenf@outlook.com>
2017-10-24 15:40:42 -0200

    <@KarenF>#6 adiciona carrossel

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -3,7 +3,7 @@ function carousel() {
 
     let list = document.querySelectorAll("li");
 
-    for (let i = 0; i < list.length; ++i) {
+    for (let i = 0; i < 3; ++i) {
       list[i].classList.toggle('carousel-invisible')
       list[i].classList.toggle('carousel-visible')
     }
